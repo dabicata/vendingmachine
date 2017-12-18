333e18b8b9fb49bcf0d4fc0bee56b44344f07ffd
dabicata <dabicata@gmail.com>
2017-12-18 18:34:36 +0200

    Working On Gui

--- model/DAO/MachineDAO.php
+++ model/DAO/MachineDAO.php
@@ -53,8 +53,8 @@ class MachineDAO implements CRUDInterface
      */
     public function insert(iterable $insertParam)
     {
-        $sql = 'INSERT INTO `vending_machines` (`vending_machine_rows`, `vending_machine_columns`, `machine_size`, `vending_machine_date_created`)' .
-            'VALUES (?, ?, ?, now())';
+        $sql = 'INSERT INTO `vending_machines` (`vending_machine_rows`, `vending_machine_columns`, `machine_size`, `vending_machine_status_id`, `vending_machine_date_created`)' .
+            'VALUES (?, ?, ?, ?, now())';
         $db = new DbConnector();
         $data = $db->executeQuery($sql, $insertParam);
         $db->closeConnection();
@@ -70,7 +70,7 @@ class MachineDAO implements CRUDInterface
      */
     public function update(iterable $updateParam)
     {
-        $sql = 'UPDATE `vending_machines` SET `vending_machine_rows` = ?, `vending_machine_columns` = ?, `machine_size` = ?,`vending_machine_date_updated` = now()' .
+        $sql = 'UPDATE `vending_machines` SET `vending_machine_rows` = ?, `vending_machine_columns` = ?, `machine_size` = ?,`vending_machine_status_id` = ?, `vending_machine_date_updated` = now()' .
             ' WHERE `vending_machines`.`vending_machine_id` = ?';
         $db = new DbConnector();
         $db->executeQuery($sql, $updateParam);
